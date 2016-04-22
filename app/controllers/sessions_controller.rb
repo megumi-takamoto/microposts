994971b6c2fe3b1a4233a4df7d7d8bd2d5282b27
sie nishizaki <mem7150@gmail.com>
2016-04-22 07:25:43 +0000

    add followings/followers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,13 @@
-class SessionsController < ApplicationController
-  def new
-  end
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      flash[:info] = "logged in as #{@user.name}"
-      redirect_to @user
-    else
-      flash[:danger] = 'invalid email/password combination'
-      render 'new'
-    end
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
   end
-  
+
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    @user = current_user.following_relationships.find(params[:id]).followed
+    current_user.unfollow(@user)
   end
 end
